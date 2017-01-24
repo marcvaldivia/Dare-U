d091f4f50e898e6d2a5afd0375f59928de086939
Marc Valdivia <marcvaldivia.developer@gmail.com>
2017-01-24 12:50:46 +0100

    The configuration variables may be hidden

--- PHP/constants/config.php
+++ PHP/constants/config.php
@@ -3,15 +3,15 @@
 	/**
 	 * Database config variables
 	 */
-	define('DB_HOST', 'mysql.hostinger.es');
-	define('DB_USER', 'u437297916_dareu');
-	define('DB_PASSWORD', '4rY1H694rMbZ');
-	define('DB_DATABASE', 'u437297916_dareu');
+	define('DB_HOST', '**************');
+	define('DB_USER', '**************');
+	define('DB_PASSWORD', '**************');
+	define('DB_DATABASE', '**************');
 	define('DB_SOCKET', '');
 	
 	/**
 	 * Google API Key
 	 */
-	define('GOOGLE_API_KEY', 'AIzaSyAjwM8E6WnHTiTfQ82xFmXnCe9U6K3j5A8');
+	define('GOOGLE_API_KEY', '**************');
 
 ?>
